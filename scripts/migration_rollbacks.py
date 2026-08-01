f124f8865e396affc87ec338cbe21b50433b4faf
Anurag9000 <anuragbasistha@gmail.com>
2026-08-02 02:11:36 +0530

    Add encrypted migration rollback entrypoint

---
+++ scripts/migration_rollbacks.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Operator entrypoint for encrypted migration rollback artifacts."""
+
+from tools.migration_rollback_cli import main
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
